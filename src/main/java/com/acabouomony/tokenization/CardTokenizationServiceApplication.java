8d085db8e87f08c1faf9822f401fb2a68d17458a
Isaac Conceição <60546623+isaac-conceicao@users.noreply.github.com>
2024-11-17 22:17:59 -0300

    initial commit

---
+++ src/main/java/com/acabouomony/tokenization/CardTokenizationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.acabouomony.tokenization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardTokenizationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardTokenizationServiceApplication.class, args);
+    }
+
+}
